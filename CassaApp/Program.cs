34d28666d10e23e05cbe68889ee2a07e59d31d77
Mogu4iyDed <semikozdanil@gmail.com>
2022-02-07 19:28:02 +0300

    короче тесты продолжаются

--- CassaApp/Program.cs
+++ CassaApp/Program.cs
@@ -15,6 +15,7 @@ namespace CassaApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Даша шлюз
         }
     }
 }
